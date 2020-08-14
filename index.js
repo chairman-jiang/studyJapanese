fe0812042780b9eb4ebca3447eb472e4c0742ec8
徐晨淼 <17600000559@163.com>
2020-08-14 18:00:39 +0800

    add test mode

--- index.js
+++ index.js
@@ -32,4 +32,63 @@
     tableFragment.appendChild(tableRow);
   });
   baseWordView.appendChild(tableFragment);
+
 })(baseObj);
+
+
+let isShowHiragana = true
+let isShowKatakana = true;
+let isShowRomaji = true;
+
+function handleTestDialog (e) {
+  let dialog = document.querySelector('.dialog-view');
+  dialog.setAttribute('style', 'display: flex;')
+  dialog.className = 'dialog-view show-dialog';
+  handleNextCardItem()
+}
+
+function handleDialogClose () {
+  let dialog = document.querySelector('.dialog-view');
+  dialog.setAttribute('style', 'display: none;')
+}
+
+function handlePrveCardItem () {}
+
+function handleNextCardItem () {
+  let dialog = document.querySelector('.dialog-view');
+  let hiraganaDom = dialog.querySelector('.hiragana');
+  let katakanaDom = dialog.querySelector('.katakana');
+  let romajiDom = dialog.querySelector('.romaji');
+  
+  hiraganaDom.setAttribute('style', `display: ${isShowHiragana ? 'block' : 'none'};`)
+  katakanaDom.setAttribute('style', `display: ${isShowKatakana ? 'block' : 'none'};`)
+  romajiDom.setAttribute('style', `display: ${isShowRomaji ? 'block' : 'none'};`)
+  let list = baseObj.base.flat();
+  let rad = Math.floor(Math.random() * list.length);
+  let curData = list[rad];
+  
+  hiraganaDom.innerHTML = curData.hiragana;
+  katakanaDom.innerHTML = curData.katakana;
+  romajiDom.innerHTML = curData.rome;
+}
+
+function handleToggleHiragana (e) {
+  isShowHiragana = !isShowHiragana;
+  e.srcElement.innerHTML = `${isShowHiragana ? '隐藏' : '显示'}平假名`;
+  let hiraganaDom = document.querySelector('.hiragana');
+  hiraganaDom.setAttribute('style', `display: ${isShowHiragana ? 'block' : 'none'};`)
+}
+
+function handleToggleKatakana (e) {
+  isShowKatakana = !isShowKatakana;
+  e.srcElement.innerHTML = `${isShowKatakana ? '隐藏' : '显示'}片假名`
+  let katakanaDom = document.querySelector('.katakana');
+  katakanaDom.setAttribute('style', `display: ${isShowKatakana ? 'block' : 'none'};`)
+}
+
+function handleToggleRomaji (e) {
+  isShowRomaji = !isShowRomaji;
+  e.srcElement.innerHTML = `${isShowRomaji ? '隐藏' : '显示'}罗马音`
+  let romajiDom = document.querySelector('.romaji');
+  romajiDom.setAttribute('style', `display: ${isShowRomaji ? 'block' : 'none'};`)
+}
