21fdc9a0d45785ff4b93d7146aab04f764aa4854
徐晨淼 <17600000559@163.com>
2020-11-26 13:43:53 +0800

    适配移动端

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,18 @@
+// const HtmlWebpackPlugin = require('')
+// const path = require('path');
+
+// const resolvePath = (src) => {
+//   return path.resolve(`../${src}`)
+// }
 
 module.exports = {
   mode: 'development',
-  entry: ''
+  entry: '/index.js',
+  output: {
+    name: ''
+  },
+  devServer: {
+    open: true,
+    host: '0.0.0.0'
+  }
 }
